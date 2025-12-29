f6bc524a340fbef1aeeae231c93f928eb8913714
Vibecore2007 <neeraj11meenaa22@gmail.com>
2025-12-29 18:19:39 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,17 @@
+const CACHE = "miya-cache-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./brain.js",
+  "./brain.worker.js",
+  "./manifest.webmanifest"
+];
 
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((hit) => hit || fetch(e.request)));
+});
